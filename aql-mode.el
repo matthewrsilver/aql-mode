5a10a2943ae7907c443cae578e8d0160f69cdb23
matthewrsilver <matthew.r.silver@gmail.com>
2016-07-28 21:16:51 -0400

    Basic indentation support

--- aql-mode.el
+++ aql-mode.el
@@ -9,9 +9,88 @@
 ;;; License: MIT License -- see LICENSE.txt
 
 ;; TODO:
-;;   - line indentation (indent after for, "{", and "(" )
 ;;   - built-in functions?
 ;;   - namespaces?
+
+
+;; Indentation
+;;
+;; The indentation component consists of  a single function aql-indent-line that
+;; is called whenever  emacs needs to determine  how to indent a  line.
+
+(setq default-tab-width 4)
+
+;; Indent the current line
+(defun aql-indent-line ()
+  "Indent current line as AQL code"
+  (interactive)
+  (beginning-of-line)
+
+  ;; if the point is at the beginning of the buffer indent the line to column 0
+  (if (bobp)
+    (indent-line-to 0)
+
+    ;; else, define some variables that will be useful as we investigate further
+    (let ( (not-indented t) (cur-indent nil) )
+
+      ;; if the current line is a close brace then we can indent it by subtracting
+      ;; the tab-width from the indent of the previous line
+      (if (looking-at "^[ \t]*[)}]")
+
+          (progn ;; evaluate the following commands in sequence...
+
+            ;; set cur-indent to the indent of the previous line minus tab-width
+            (save-excursion
+              (forward-line -1)
+              (setq cur-indent (- (current-indentation) default-tab-width))
+              )
+
+            ;; rectify cur-indent
+            (if (< cur-indent 0) (setq cur-indent 0) )
+
+            ) ;; end progn
+
+        ;; else (i.e. if the current line is not a close brace) we'll need to
+        ;; walk through the previous lines, searching for information that can
+        ;; tell us what the indentation for the current line should be.
+        (save-excursion
+
+          ;; while we have not yet indented the current line
+          (while not-indented
+
+            ;; move to the previous line
+            (forward-line -1)
+
+            ;; if the line is a close brace, then we know that the current line
+            ;; should be indented to match.
+            (if (looking-at "^[ \t]*[)}]")
+                (progn
+                  (setq cur-indent (current-indentation))
+                  (setq not-indented nil))
+
+              ;; else if the line is a block-initiating line -- e.g. it is a
+              ;; for loop, or an open brace (TODO buggy) set the current line
+              ;; should be indented one tab-width forward
+              (if (looking-at "^[ \t]*\\(FOR\\|for\\|.*[({]$\\)")
+                  (progn
+                    (setq cur-indent (+ (current-indentation) default-tab-width))
+                    (setq not-indented nil))
+
+                ;; else if we've reached the beginning of the buffer (base case),
+                ;; then we don't indent the line at all.
+                (if (bobp)
+                    (setq not-indented nil))))))
+
+        ) ;; end initial line check
+
+      ;; now apply the indentation
+      (if cur-indent
+          (indent-line-to cur-indent)
+        (indent-line-to 0))
+
+      )))
+
+
 
 ;; define keyword categories
 (setq aql-keywords
@@ -48,8 +127,8 @@
   "Major mode for editing Arango Query Language (AQL)"
 
   (setq font-lock-defaults '((aql-font-lock-keywords)))
-  (set (make-local-variable 'font-lock-defaults)
-       '(aql-font-lock-keywords nil t))
+  (set (make-local-variable 'font-lock-defaults) '(aql-font-lock-keywords nil t))
+  (set (make-local-variable 'indent-line-function) 'aql-indent-line)
   )
 
 ;; clear memory
